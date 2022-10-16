9b61e5a2093b17afe7f3cd5b838e95b7592f074d
Satwik Pattanaik <34216452+satwik18@users.noreply.github.com>
2022-10-16 05:01:07 -0400

    Sp comment section integration
    * Working Javascript
    
    * Hide elements

--- utils/resource_loader.js
+++ utils/resource_loader.js
@@ -1,5 +1,5 @@
 export default class Helpers {
-  static loadHtmlDoc(path) {
+  static loadHtmlBody(path, callback) {
     let client = new XMLHttpRequest();
     client.open("GET", chrome.runtime.getURL(path));
     client.onreadystatechange = function () {
@@ -9,7 +9,24 @@ export default class Helpers {
           "text/html"
         );
         let element = html.body.firstChild;
-        document.body.insertAdjacentElement("beforeend", element);
+        callback(element);
+      }
+    };
+    client.send();
+  }
+
+  static loadHtmlHead(path, callback) {
+    let client = new XMLHttpRequest();
+    client.open("GET", chrome.runtime.getURL(path));
+    client.onreadystatechange = function () {
+      if (client.readyState == client.DONE) {
+        const html = new DOMParser().parseFromString(
+          client.responseText,
+          "text/html"
+        );
+        for (let elem of html.head.children) {
+          callback(elem);
+        }
       }
     };
     client.send();
